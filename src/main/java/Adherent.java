87e4941572daf7a4286129f57170e92337f45a8b
Hajar AKBOUB <116205242+akboubhajar@users.noreply.github.com>
2024-02-27 20:57:08 +0100

    Update Adherent.java

--- src/main/java/Adherent.java
+++ src/main/java/Adherent.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Adherent {
+
+class Adherent extends Personne {
+    private int numAdherent;
+
+    // Constructeur avec paramètres
+    public Adherent(String nom, String prenom, String email, String tel, int age, int numAdherent) {
+        super(nom, prenom, email, tel, age);
+        this.numAdherent = numAdherent;
+    }
+
+    // Redéfinition de la méthode toString
+    public String toString() {
+        return super.toString() + ", Numéro Adhérent: " + numAdherent;
+    }
 }
